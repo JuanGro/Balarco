8f9a5a762333f68ca62b362ecb6bccea688927ba
JuanGro <a01207672@itesm.mx>
2017-02-17 13:42:16 -0600

    Fixes testing configuration

---
+++ src/app/login/login.component.spec.ts
@@ -0,0 +1,28 @@
+import {
+    TestBed
+} from '@angular/core/testing';
+
+import {
+    FormGroup,
+    ReactiveFormsModule
+} from '@angular/forms';
+
+import { LoginComponent } from './login.component';
+
+describe('Component: LoginComponent', () => {
+    let component: LoginComponent;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [LoginComponent],
+            imports: [ReactiveFormsModule]
+        });
+
+        const fixture = TestBed.createComponent(LoginComponent);
+        component = fixture.componentInstance;
+    });
+
+    it('should have a defined component', () => {
+        expect(component).toBeDefined();
+    });
+});
