3422379702371b3cddc8708034d4d1acd80d4853
EduardoVaca <vacalalo@gmail.com>
2017-02-22 12:34:16 -0600

    Add comments in Login Tests

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -17,6 +17,7 @@ describe('Login Component', () => {
 
   beforeEach(() => {
 
+    // Mock service, avoids requirements in providing all HttpService dependencies.
     httpServiceStub = {
       isLoggedIn: true,
       user: { name: 'Test User'}
@@ -28,6 +29,7 @@ describe('Login Component', () => {
       providers: [ {provide: HttpService, useValue: httpServiceStub }]
     });
 
+    // UserService actually injected into the component
     httpService = TestBed.get(HttpService);
 
     fixture = TestBed.createComponent(LoginComponent);
@@ -37,30 +39,36 @@ describe('Login Component', () => {
   });
 
   /**
-  * Tests the mock provider for the test
+  * Tests the mock provider for the test.
   **/
   it('stub object and injected HttpService should not be the same', () => {
     expect(httpServiceStub === httpService).toBe(false);
 
-    // Changing the stub object has no effect on the injected service
+    // Changing the stub object has no effect on the injected service.
     httpServiceStub.isLoggedIn = false;
     expect(httpService.isLoggedIn).toBe(true);
   });
 
   /**
-  * Tests the interface creation by the component
+  * Tests the interface consistance with LoginComponent.
   **/
   it('should display original title', () => {
     fixture.detectChanges();
     expect(el.textContent).toContain(comp.title);
   });
 
+  /**
+  * Tests changes in component reflects in interface.
+  **/
   it('should display a different test title', () => {
     comp.title = 'Test Title';
     fixture.detectChanges();
     expect(el.textContent).toContain('Test Title');
   });
 
+  /**
+  * Tests changes in component reflects in interface and component changes kept. 
+  **/
   it('should display updated title after detectChanges', () => {
     comp.title = 'Test Title';
     fixture.detectChanges(); // detect changes explicitly
