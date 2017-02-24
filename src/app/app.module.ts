d4c695e48469002bf57541e4a6f804be4c5ad537
EduardoVaca <vacalalo@gmail.com>
2017-02-24 16:40:47 -0600

    Delete first test example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,7 +43,7 @@ import { SimpleLayoutComponent } from './layouts/simple-layout.component';
     SIDEBAR_TOGGLE_DIRECTIVES,
     AsideToggleDirective
   ],
-  providers: [ // CompletedDesignsListService,
+  providers: [
   {
     provide: LocationStrategy,
     useClass: HashLocationStrategy
