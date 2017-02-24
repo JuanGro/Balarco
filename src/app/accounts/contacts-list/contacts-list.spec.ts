e7d30ef0f116e1f9eb9e8a25f77c80549753ade7
JuanGro <a01207672@itesm.mx>
2017-02-23 19:00:02 -0600

    Add title to contacts list page

--- src/app/accounts/contacts-list/contacts-list.spec.ts
+++ src/app/accounts/contacts-list/contacts-list.spec.ts
@@ -1,3 +1,9 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By }              from '@angular/platform-browser';
+import { DebugElement }    from '@angular/core';
+
+import { ContactsListComponent } from './contacts-list.component';
+
 describe('1st tests', () => {
   it('true is true', () => expect(true).toBe(true));
 });
