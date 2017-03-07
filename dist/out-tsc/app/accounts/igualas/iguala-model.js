e90a25fd1dd3daf7a562261ac793786edb539c3a
EduardoVaca <vacalalo@gmail.com>
2017-03-06 19:52:42 -0600

    Add Iguala model class

---
+++ dist/out-tsc/app/accounts/igualas/iguala-model.js
@@ -0,0 +1,7 @@
+var Iguala = (function () {
+    function Iguala() {
+    }
+    return Iguala;
+}());
+export { Iguala };
+//# sourceMappingURL=../../../../../src/app/accounts/igualas/iguala-model.js.map
---
+++ src/app/accounts/igualas/iguala-model.ts
@@ -0,0 +1,10 @@
+/**
+* Model that defines an Iguala.
+**/
+export class Iguala {
+  id?: number;
+  name: string;
+  start_date: Date;
+  end_date: Date;
+  // TODO: Add client attribute after merge with develop.
+}
