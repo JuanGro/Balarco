2a03c245545acd472ff804d7f8452d21b40e117a
EduardoVaca <vacalalo@gmail.com>
2017-03-15 17:17:06 -0600

    Delete dist forlder

--- dist/out-tsc/environments/environment.js
+++
@@ -1,8 +0,0 @@
-export var environment = {
-    production: false,
-    'API_URL': 'http://127.0.0.1:8000/',
-    'CLIENTS_URL': 'clients/clients/',
-    'CONTACTS_URL': 'clients/contacts/',
-    'IGUALAS_URL': 'works/igualas/',
-};
-//# sourceMappingURL=../../../src/environments/environment.js.map
