c3c700b9f818c3be486061614784f8f293bab8df
juanGro <ing.juangro@gmail.com>
2017-03-16 14:20:30 -0600

    Change environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  'API_URL': 'http://198.199.95.61/api/',
+  'API_URL': 'http://198.199.95.61/',
   'CLIENTS_URL': 'clients/clients/',
   'CONTACTS_URL': 'clients/contacts/',
   'IGUALAS_URL': 'works/igualas/'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  'API_URL': 'http://198.199.95.61/api/',
+  'API_URL': 'http://198.199.95.61/',
   'CLIENTS_URL': 'clients/clients/',
   'CONTACTS_URL': 'clients/contacts/',
   'IGUALAS_URL': 'works/igualas/',
