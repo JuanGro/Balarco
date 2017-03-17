08898bbefa16ebabcf9e4392db4ccf85e62106db
EduardoVaca <vacalalo@gmail.com>
2017-03-16 19:47:49 -0600

    Add missing attribute of ArtWork in its tests

--- dist/out-tsc/app/works/art-works/art-work.model.spec.js
+++ dist/out-tsc/app/works/art-works/art-work.model.spec.js
@@ -1,30 +1,37 @@
 import { ArtWork } from './art-work.model';
 describe('ArtWork object model tests.', function () {
     it('Should create an ArtWork object by setting attibutes', function () {
-        var name = 'MyArt';
         var id = 1;
+        var name = 'MyArt';
+        var quantity = 15;
         var artWork = {
             id: id,
             name: name,
+            quantity: quantity
         };
         expect(artWork.id).toBe(id);
         expect(artWork.name).toBe(name);
+        expect(artWork.quantity).toBe(quantity);
     });
     it('Should create an empty Artwork object', function () {
         var artWork = new ArtWork();
         expect(artWork.id).toBe(undefined);
         expect(artWork.name).toBe(undefined);
+        expect(artWork.quantity).toBe(undefined);
     });
     it('Should create an ArtWork object by passing a Json in the constructor', function () {
-        var name = 'MyArt';
         var id = 1;
+        var name = 'MyArt';
+        var quantity = 15;
         var jsonArtWork = {
             id: id,
-            name: name
+            name: name,
+            quantity: quantity
         };
         var artWork = new ArtWork(jsonArtWork);
         expect(artWork.id).toBe(id);
         expect(artWork.name).toBe(name);
+        expect(artWork.quantity).toBe(quantity);
     });
 });
 //# sourceMappingURL=../../../../../src/app/works/art-works/art-work.model.spec.js.map
--- src/app/works/art-works/art-work.model.spec.ts
+++ src/app/works/art-works/art-work.model.spec.ts
@@ -4,16 +4,19 @@ import { ArtWork } from './art-work.model';
 describe('ArtWork object model tests.', () => {
 
   it('Should create an ArtWork object by setting attibutes', () => {
-    let name = 'MyArt';
     let id = 1;
+    let name = 'MyArt';
+    let quantity = 15;
 
     let artWork: ArtWork = {
       id: id,
       name: name,
+      quantity: quantity
     };
 
     expect(artWork.id).toBe(id);
     expect(artWork.name).toBe(name);
+    expect(artWork.quantity).toBe(quantity);
   });
 
 
@@ -22,21 +25,25 @@ describe('ArtWork object model tests.', () => {
 
     expect(artWork.id).toBe(undefined);
     expect(artWork.name).toBe(undefined);
+    expect(artWork.quantity).toBe(undefined);
   });
 
 
   it('Should create an ArtWork object by passing a Json in the constructor', () => {
-    let name = 'MyArt';
     let id = 1;
+    let name = 'MyArt';
+    let quantity = 15;
 
     let jsonArtWork = {
       id: id,
-      name: name
+      name: name,
+      quantity: quantity
     };
 
     let artWork = new ArtWork(jsonArtWork);
     expect(artWork.id).toBe(id);
     expect(artWork.name).toBe(name);
+    expect(artWork.quantity).toBe(quantity);
   });
 
 });
