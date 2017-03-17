043c8016900c589dbd33fe6b834a95d80bbdfad9
EduardoVaca <vacalalo@gmail.com>
2017-03-16 19:17:08 -0600

    Add work-type class

---
+++ src/app/works/work-type/work-type.model.ts
@@ -0,0 +1,13 @@
+/**
+* Class that represents a Work type:
+* Iguala, Graduations or Projects.
+**/
+export class WorkType {
+  id?: number;
+  name?: string;
+
+  constructor(object?: any) {
+    this.id = object && object.id;
+    this.name = object && object.name;
+  }
+}
