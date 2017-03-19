5c9d542ab663fea1358210a8bae46d9540e56e33
juanGro <ing.juangro@gmail.com>
2017-03-19 17:49:10 -0600

    Create new route and folder

---
+++ src/app/general-user/general-user.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { DropdownModule } from 'ng2-bootstrap/dropdown';
+import { CommonModule } from '@angular/common';
+import { ModalModule } from 'ng2-bootstrap/modal';
+import { ToasterModule } from 'angular2-toaster/angular2-toaster';
+
+// Components
+import { GeneralUserRoutingModule } from './general-user-routing.module';
+import { WorksComponent } from './works/works.component';
+
+// Datepicker
+import { DatepickerModule } from 'ng2-bootstrap/datepicker';
+import { SelectModule } from 'ng2-select';
+
+// Services
+import { HttpService } from '../shared/http-service/http.service';
+import { CanActivateAuthGuard } from '../shared/auth/auth.service';
+import { CustomToastService } from '../shared/toast/custom-toast.service';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    GeneralUserRoutingModule,
+    ChartsModule,
+    DropdownModule,
+    ModalModule.forRoot(),
+    DatepickerModule.forRoot(),
+    SelectModule,
+    ToasterModule
+  ],
+  declarations: [ WorksComponent ],
+
+  providers: [ CanActivateAuthGuard, HttpService, CustomToastService ]
+})
+export class GeneralUserModule { }
---
+++ src/app/general-user/works/works.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'works',
+  templateUrl: 'works.component.html'
+})
+/**
+* Component which manage the administration of works:
+* - List all the works.
+* - Create a new work.
+* - Update an specific work.
+* - Remove a work.
+**/
+export class WorksComponent {
+    public constructor() {
+
+    }
+}
