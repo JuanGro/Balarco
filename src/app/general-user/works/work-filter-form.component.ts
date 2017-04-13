89cee7e6f2b778c64fcf843ba445eec55600197b
juanGro <ing.juangro@gmail.com>
2017-04-12 19:31:35 -0500

    Pull data for dropdowns

--- src/app/general-user/works/work-filter-form.component.ts
+++ src/app/general-user/works/work-filter-form.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnChanges, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnChanges, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 // Models
 import { WorkFilter } from './work-filter.model';
+import { Client } from '../../accounts/clients/client-model';
+import { Contact } from '../../accounts/contacts/contact-model';
+import { Iguala } from '../../accounts/igualas/iguala-model';
+import { Status } from './status/status-model';
 
 @Component({
   selector: 'work-filter-form',
@@ -17,17 +21,32 @@ import { WorkFilter } from './work-filter.model';
 * - Request actions in modals to the parent component.
 **/
 export class WorkFilterFormComponent implements OnChanges, OnInit {
-    public workFilter: WorkFilter;
-    // Variable to active the form.
-    public active: boolean = true;
+    // Receives the contacts list from parent component.
+    @Input('contactsList') contactsList: Contact[];
+    // Receives the clients list from parent component.
+    @Input('clientsList') clientsList: Client[];
+    // Receives the igualas list from parent component.
+    @Input('igualasList') igualasList: Iguala[];
+    // Receives status list from parent component.
+    @Input('statusList') statusList: Status[];
     // Requests close of the current modal to parent component.
     @Output() requestCloseModal: EventEmitter<string> = new EventEmitter();
     // Variable to check in test what action is executed between components.
     public modalAction: string = '';
+    // Variable to control the work filter.
+    public workFilter: WorkFilter;
+    // Variable to active the form.
+    public active: boolean = true;
+    // Variable for filtering Contacts by Client selected in dropown..
+    private currentContacts: Contact[];
+    // Variable to set the client when an existing work is received.
+    private client_id: number;
+    // Variable to set the contact when client dropdown changes.
+    private contact_id: number;
 
     public ngOnInit() {
-        console.log('start');
         this.workFilter = new WorkFilter();
+        this.initialDropdownSetup();
     }
 
     /**
@@ -37,7 +56,40 @@ export class WorkFilterFormComponent implements OnChanges, OnInit {
      **/
     public ngOnChanges() {
         this.workFilter = new WorkFilter();
-        console.log('hi', this.workFilter);
+        this.initialDropdownSetup();
+    }
+
+    /**
+     * Method to fill dropdowns initially when the modal is shown and Work is new.
+     **/
+    private initialDropdownSetup() {
+        if (this.clientsList && this.contactsList && this.clientsList.length > 0) {
+            this.client_id = this.clientsList[0].id;
+            this.filterContactsByClientId(this.client_id);
+        }
+    }
+
+    /**
+     * Method that listens to event of change in the Clients dropdown.
+     * Params:
+     *   - id: Id of the new client selected.
+     **/
+    public onClientChange(id: number) {
+        this.filterContactsByClientId(id);
+    }
+
+    /**
+     * Method that filters the contacts by the client id.
+     * Params:
+     *   - id: Id of the client from which the contacts will be filtered.
+     **/
+    private filterContactsByClientId(id: number) {
+        if (this.contactsList) {
+            this.currentContacts = this.contactsList.filter(x => x.client === +id);
+            if (this.currentContacts.length > 0) {
+            this.contact_id = this.currentContacts[0].id;
+            }
+        }
     }
 
     /**
