d5240a0f3d8598429fce3573d362ff9f85dd6318
EduardoVaca <vacalalo@gmail.com>
2017-04-15 19:21:33 -0500

    Add enum for User Role

---
+++ dist/out-tsc/app/shared/auth/role.js
@@ -0,0 +1,11 @@
+var Role;
+(function (Role) {
+    Role[Role["DIRECTOR_CUENTAS"] = 0] = "DIRECTOR_CUENTAS";
+    Role[Role["EJECUTIVO"] = 1] = "EJECUTIVO";
+    Role[Role["VENTAS"] = 2] = "VENTAS";
+    Role[Role["DIRECTOR_ARTE"] = 3] = "DIRECTOR_ARTE";
+    Role[Role["DISENADOR_SR"] = 4] = "DISENADOR_SR";
+    Role[Role["DISENADOR_JR"] = 5] = "DISENADOR_JR";
+    Role[Role["SUPER_USUARIO"] = 6] = "SUPER_USUARIO";
+})(Role || (Role = {}));
+//# sourceMappingURL=../../../../../src/app/shared/auth/role.js.map
---
+++ src/app/shared/auth/role.ts
@@ -0,0 +1,12 @@
+/**
+* Enum to keep track of the role the current user might have.
+**/
+enum Role {
+  DIRECTOR_CUENTAS,
+  EJECUTIVO,
+  VENTAS,
+  DIRECTOR_ARTE,
+  DISENADOR_SR,
+  DISENADOR_JR,
+  SUPER_USUARIO
+}
