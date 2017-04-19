6accc71a1a8a534b751f5b08a112eea6375a6021
juanGro <ing.juangro@gmail.com>
2017-04-19 18:41:44 -0500

    Build form and implement submitform method

--- src/app/login/recover-password-form.component.ts
+++ src/app/login/recover-password-form.component.ts
@@ -1,9 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
+
+// Components
+import { Login } from './login';
 
 @Component({
     selector: 'recover-password-form',
     templateUrl: 'recover-password-form.component.html'
 })
-export class RecoverPasswordFormComponent {
-    @Input('username') username: string;
+export class RecoverPasswordFormComponent implements OnInit {
+    // Login object for the ngForm handle.
+    public recoverPassword: Login;
+
+    /**
+      * Builds the component for first time each time when it's called.
+      *   - Initialize the recoverPassword object for the form.
+      **/
+    ngOnInit() {
+        this.recoverPassword = new Login();
+    }
+
+    /**
+      * Method for sending the username to request password and reset the form.
+      * Params:
+      *   - object: Takes the username from form.
+      **/
+    submitRecoverPasswordForm(form: NgModel, object: Login) {
+        console.log(object);
+        form.reset();
+    }
 }
