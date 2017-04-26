c46912be6433842e7d93e7e582bd312645fb2f4a
juanGro <ing.juangro@gmail.com>
2017-04-26 00:35:41 -0500

    Remove unused methods and variables

--- src/app/design-director/assignments/assignments.component.ts
+++ src/app/design-director/assignments/assignments.component.ts
@@ -5,14 +5,8 @@ import { HttpService } from './../../shared/http-service/http.service';
 import { CustomToastService } from '../../shared/toast/custom-toast.service';
 
 // Models
-import { ArtWork } from '../../general-user/works/art-works/art-work-model';
-import { Client } from '../../accounts/clients/client-model';
 import { User } from '../../admin/users/user-model';
-import { Contact } from '../../accounts/contacts/contact-model';
-import { Iguala } from '../../accounts/igualas/iguala-model';
-import { Status } from '../../general-user/works/status/status-model';
 import { Work } from '../../general-user/works/work-model';
-import { WorkType } from '../../general-user/works/work-type/work-type-model';
 import { Designer } from '../../general-user/works/designer/designer-model';
 import { URLSearchParams } from '@angular/http';
 
@@ -39,20 +33,8 @@ export class AssignmentsComponent implements OnInit {
   public worksList: Work[];
   // Variable to save the complete work list.
   public completeWorksList: Work[];
-  // Variable that saves all clients from DB.
-  public clientsList: Client[];
-  // Variable that saves all contacts from DB.
-  public contactsList: Contact[];
-  // List of igualas received from httpService.
-  public igualasList: Iguala[];
-  // List of Work Types received from httpService.
-  public workTypesList: WorkType[];
-  // List of Work Types of graduation only.
-  public graduationArtTypes: ArtWork[];
   // Variable that saves all users from DB.
   public userList: User[];
-  // List of Status.
-  public statusList: Status[];
   // List of designers
   public designerListDefault: Designer[];
   // Title of update work modal.
@@ -71,13 +53,7 @@ export class AssignmentsComponent implements OnInit {
     this.designerListDefault = [];
 
     this.loadWorksList(environment.WORKS_URL);
-    this.loadClientsList(environment.CLIENTS_URL);
-    this.loadContactsList(environment.CONTACTS_URL);
-    this.loadIgualasList(environment.IGUALAS_URL);
     this.loadUserList(environment.USERS_URL);
-    this.loadWorkTypesList(environment.WORK_TYPES_URL);
-    this.loadWorkTypesForGraduation(environment.ART_TYPES_URL);
-    this.loadStatusList(environment.STATUS_URL);
   }
 
   /**
@@ -99,67 +75,6 @@ export class AssignmentsComponent implements OnInit {
                     error => {
                       this.toaster.show(error, 'Error', 'Ocurrió un error al cargar los trabajos');
                     });
-  }
-
-  /**
-  * Loads all the clients from the get method in httpService to use it the client attribute of the current component.
-  * Params:
-  *   - url: The url where the service will comunicate to get the Client object.
-  **/
-  private loadClientsList(url: string) {
-    this.httpService.getObject(url)
-                    .map((data: any) => data.json())
-                    .subscribe(clientsListJSON => {
-                      // Creates clients objects from JSON.
-                      this.clientsList = [];
-                      for (let clientJSON of clientsListJSON) {
-                        this.clientsList.push(new Client(clientJSON));
-                      }
-                    },
-                    error => {
-                      this.toaster.show(error, 'Error', 'Ocurrió un error al cargar los clientes');
-                    });
-  }
-
-  /**
-  * Loads all the contacts from the get method in httpService.
-  * Params:
-  *   - url: The url where the service will comunicate to get the Contact object.
-  **/
-  private loadContactsList(url: string) {
-    this.httpService.getObject(url)
-                    .map((data: any) => data.json())
-                    .subscribe(contactsListJSON => {
-                      // Creates Contact objects from JSON.
-                      this.contactsList = [];
-                      for (let contactJSON of contactsListJSON) {
-                        this.contactsList.push(new Contact(contactJSON));
-                      }
-                    },
-                    error => {
-                      this.toaster.show(error, 'Error', 'Ocurrió un error al cargar los contactos');
-                    });
-  }
-
-  /**
-  * Loads all the igualas with httpService.
-  * Params:
-  *   - url: Url to igualas API methods.
-  **/
-  private loadIgualasList(url: string) {
-    this.httpService.getObject(url)
-                    .map((data: any) => data.json())
-                    .subscribe(igualasListJSON => {
-                      // Creates Iguala objects from JSON.
-                      this.igualasList = [];
-                      for (let igualaJSON of igualasListJSON) {
-                        this.igualasList.push(new Iguala(igualaJSON));
-                      }
-                    },
-                      error => {
-                        this.toaster.show(error, 'Error', 'Ocurrió un error al cargar las igualas');
-                      });
-
   }
 
   /**
@@ -189,68 +104,6 @@ export class AssignmentsComponent implements OnInit {
                     },
                     error => {
                       this.toaster.show(error, 'Error', 'Ocurrió un error al cargar los usuarios');
-                    });
-  }
-
-  /**
-  * Loads all the workTypes with httpService.
-  * Params:
-  *   - url: Url to work types API methods.
-  **/
-  private loadWorkTypesList(url: string) {
-    this.httpService.getObject(url)
-                    .map((data: any) => data.json())
-                    .subscribe(workTypesListJSON => {
-                      // Creates WorkType objects from JSON
-                      this.workTypesList = [];
-                      for (let workTypeJSON of workTypesListJSON) {
-                        this.workTypesList.push(new WorkType(workTypeJSON));
-                      }
-                    },
-                    error => {
-                      this.toaster.show(error, 'Error', 'Ocurrió un error al cargar los tipos de trabajo');
-                    });
-  }
-
-  /**
-  * Loads all the art types for Graduation only with httpService.
-  * Params:
-  *   - url: Url to art types API methods.
-  **/
-  private loadWorkTypesForGraduation(url: string) {
-    let graduation_id = '2'; // Should be id of graduación in API.
-    let params = new URLSearchParams();
-    params.set('work_work_type_id', graduation_id);
-    this.httpService.getObject(url, params)
-                    .map((data: any) => data.json())
-                    .subscribe(graduationArtTypesJSON => {
-                      this.graduationArtTypes = [];
-                      for (let artType of graduationArtTypesJSON) {
-                        this.graduationArtTypes.push(new ArtWork(artType));
-                      }
-                    },
-                    error => {
-                      this.toaster.show(error, 'Error', 'Ocurrió un error al cargar las graduaciones');
-                    });
-
-  }
-
-  /**
-  * Loads all the Status with httpService.
-  * Params:
-  *   - url: Url to Status API methods.
-  **/
-  private loadStatusList(url: string) {
-    this.httpService.getObject(url)
-                    .map((data: any) => data.json())
-                    .subscribe(statusListJSON => {
-                      this.statusList = [];
-                      for (let status of statusListJSON) {
-                        this.statusList.push(new Status(status));
-                      }
-                    },
-                    error => {
-                      this.toaster.show(error, 'Error', 'Ocurrió un error al cargar los estados');
                     });
   }
 
